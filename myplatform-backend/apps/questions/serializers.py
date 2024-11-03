5789891c3f138e05e4071040f8363b365e3279bd
boghtml <boghtml@gmail.com>
2024-11-03 12:58:38 +0200

    Answer details view updateing

--- myplatform-backend/apps/questions/serializers.py
+++ myplatform-backend/apps/questions/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from .models import Question, Answer
+from apps.users.models import CustomUser 
 
 class AnswerSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
-    
+    profile_image_url = serializers.SerializerMethodField() 
+
     class Meta:
         model = Answer
-        fields = ['id', 'user', 'content', 'created_at', 'updated_at']
+        fields = ['id', 'user', 'content', 'created_at', 'updated_at', 'profile_image_url']
+
+    def get_profile_image_url(self, obj):
+        return obj.user.profile_image_url  # Повертаємо URL зображення профілю користувача
 
 class QuestionSerializer(serializers.ModelSerializer):
     teacher = serializers.StringRelatedField(read_only=True)
